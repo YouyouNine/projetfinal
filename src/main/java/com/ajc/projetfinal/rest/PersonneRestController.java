2eb28ccd9540408bd570f53b3ce5ea9cfb0e5043
Jenny <j.lambert789@gmail.com>
2022-01-12 08:17:54 +0100

    findbyUser&pass connexion

--- src/main/java/com/ajc/projetfinal/rest/PersonneRestController.java
+++ src/main/java/com/ajc/projetfinal/rest/PersonneRestController.java
@@ -3,8 +3,12 @@ package com.ajc.projetfinal.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,43 +16,56 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ajc.projetfinal.model.Personne;
 import com.ajc.projetfinal.repo.PersonneRepository;
-
+@SpringBootApplication
+@EnableJpaRepositories("com.ajc.projetfinal.repo.PersonneRepository")
 @RestController
 @RequestMapping("/rest")
 public class PersonneRestController {
-	
+
 	@Autowired
 	private PersonneRepository repo;
-	
+
 	@GetMapping("/hello")
 	public String getHello() {
-		//localhost:8080/formation/rest/hello   -> le nom formation a été ajouté dans l'application properties
+		// localhost:8080/formation/rest/hello -> le nom formation a été ajouté
+		// dans l'application properties
 		return "Hello";
 	}
-	
+
 	@GetMapping("/personnes")
 	public List<Personne> getAllPersonnes() {
 		return repo.findAll();
 	}
-	
 
-//	 @CrossOrigin
-//	@PostMapping("/personnes")
-//	public String create(@RequestBody Personne personne) {
-//		 System.out.println(personne);
-//		if (!this.repo.existsById(personne.getEmail())) {
-//			this.repo.save(personne);
-//			return "Client créé";
-//		} else {
-//			return "Erreur, Client existe déjà";
-//		}
-//	}
-	
-	 @CrossOrigin
+	// Pour la partie inscription
+	@CrossOrigin
 	@PostMapping("/personnes")
 	public void create(@RequestBody Personne personne) {
-		
-	this.repo.save(personne);
-		
+
+		this.repo.save(personne);
+
 	}
+
+	// Pour la partie connexion
+
+	@CrossOrigin
+	@GetMapping("/personnes/{username}/{password}")
+	public Personne findByUserAndPass(@PathVariable(name = "username") String username,
+			@PathVariable(name = "password") String password) {
+		return repo.findByUserAndPass(username, password);
+
+	}
+
+	// @CrossOrigin
+	// @PostMapping("/personnes")
+	// public String create(@RequestBody Personne personne) {
+	// System.out.println(personne);
+	// if (!this.repo.existsById(personne.getEmail())) {
+	// this.repo.save(personne);
+	// return "Client créé";
+	// } else {
+	// return "Erreur, Client existe déjà";
+	// }
+	// }
+
 }
--- src/main/java/com/ajc/projetfinal/service/ConsoleService.java
+++ src/main/java/com/ajc/projetfinal/service/ConsoleService.java
@@ -2,10 +2,14 @@ package com.ajc.projetfinal.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Service;
 
 import com.ajc.projetfinal.repo.PersonneRepository;
 
+@SpringBootApplication
+@EnableJpaRepositories("com.ajc.projetfinal.repo.PersonneRepository")
 @Service
 public class ConsoleService implements CommandLineRunner {
 	
