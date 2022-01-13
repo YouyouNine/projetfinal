c25c217591efc8f20309121f7db383d28ba14653
Anthony <anthodov@hotmail.fr>
2022-01-13 18:14:41 +0100

    PersonneRestController
    Ajout de la méthode update

--- src/main/java/com/ajc/projetfinal/rest/PersonneRestController.java
+++ src/main/java/com/ajc/projetfinal/rest/PersonneRestController.java
@@ -3,15 +3,11 @@ package com.ajc.projetfinal.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -74,6 +70,12 @@ public class PersonneRestController {
 		System.out.println("**************************" + p);
 		return repo.findByUsernameAndPassword(username, password);
 
+	}
+	
+	@CrossOrigin
+	@PutMapping("/personnes")
+	public void update(@RequestBody Personne personne) {
+		this.repo.save(personne);
 	}
 	
 //	@CrossOrigin
